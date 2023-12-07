2f478bad0a072d8ecafb692f2fada8226ea66c8f
Glauber Balsani <glauber_29@hotmail.com>
2023-12-07 08:53:25 -0300

    adicionando comentários

--- Program.cs
+++ Program.cs
@@ -11,6 +11,10 @@ Smartphone iphone = new Iphone("11111111", "Iphone Mil", "2222222", 128);
 iphone.Ligar();
 iphone.InstalarAplicativo("Spotify");
 
-//Apesar de não conserguimos instanciar uma classe abstrata, ela pode ser passada como um tipo
-//pois Nokie é um Smartphone, é uma relação de é um, de ser um smartphone.
-//O ideal é sempre que possível programar voltado a interface e não a implementação.
+/*
+Apesar de não conseguirmos instanciar uma classe abstrata, ela pode ser passada como um tipo.
+ Pois Nokia é um Smartphone, é uma relação de 'é um ', de ser um smartphone.
+ O ideal é sempre que possível programar voltado à interface e não à implementação 
+ */
+
+ 
